c769ed0d99fbbfb549387336a0187909c5f297f6
Tevfik <tevfikhoglu@gmail.com>
2023-03-31 15:16:32 +0300

    sprites and animation

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -12,12 +12,16 @@ public class PlayerController : MonoBehaviour
     // We'll be using this Vector2 to store our values incoming from our player controls input.
     private Vector2 movement;
     private Rigidbody2D rb;
+    private Animator myAnimator;
+    private SpriteRenderer mySpriteRenderer;
 
     private void Awake()
     {
         // Create new instance of Player Controls
         playerControls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // playerControls must be enabled in order to receive input
@@ -33,16 +37,37 @@ public class PlayerController : MonoBehaviour
     // Funfact: FixedUpdate is good for Physics!
     private void FixedUpdate()
     {
+        AdjustPlayerFacingDirection();
         Move();
     }
 
+    /// <summary>
+    /// Reads player input from PlayerControls script
+    /// movement reads only 1 and -1 for x and y
+    /// the speed is controlled by moveSpeed ın the Move() method
+    /// </summary>
     private void PlayerInput()
     {
         movement = playerControls.Movement.Move.ReadValue<Vector2>();
+
+        myAnimator.SetFloat("moveX", movement.x);
+        myAnimator.SetFloat("moveY", movement.y);
     }
 
+    /// <summary>
+    /// Moves the player based on the movement Vector2
+    /// </summary>
     private void Move()
     {
         rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+    }
+
+    /// <summary>
+    /// Adjusts the player facing direction based on the mouse position
+    /// </summary>
+    private void AdjustPlayerFacingDirection()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mySpriteRenderer.flipX = (mousePos - rb.position).x < 0;
     }
 }
